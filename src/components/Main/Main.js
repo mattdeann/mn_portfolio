4917adf292ff15394e261c4ba1858fc2cdf2bb04
mattdean <matthewallandean@gmail.com>
2021-05-09 10:41:02 -0600

    Add carousel package

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,12 +4,40 @@ import './Main.css';
 export default function Main() {
   return (
     <main>
-      <article>
+      <article className="copy-left">
         <section className="copy">
-          Esse nisi proident aute anim incididunt eu quis non. Laboris ut dolore tempor incididunt officia proident. Ad consectetur ad laboris labore id. Consectetur aliqua nulla velit sit deserunt et. Occaecat sunt velit Lorem consequat consectetur consectetur non sunt sunt. Voluptate amet incididunt excepteur consectetur dolore.
+          <h1 className="copy-header" >Energy</h1>
+          Esse nisi proident aute anim incididunt eu quis non. Laboris ut dolore tempor incididunt officia proident.
         </section>
         <section className="project-picture">
         Learn More
+        </section>
+      </article>
+      <article className="copy-right">
+        <section className="project-picture">
+        Learn More
+        </section>
+        <section className="copy">
+          <h1 className="copy-header" >Light</h1>
+          Esse nisi proident aute anim incididunt eu quis non. Laboris ut dolore tempor incididunt officia proident.
+        </section>
+      </article>
+      <article className="copy-left">
+        <section className="copy">
+          <h1 className="copy-header" >Donuts</h1>
+          Esse nisi proident aute anim incididunt eu quis non. Laboris ut dolore tempor incididunt officia proident.
+        </section>
+        <section className="project-picture">
+        Learn More
+        </section>
+      </article>
+      <article className="copy-right">
+        <section className="project-picture">
+        Learn More
+        </section>
+        <section className="copy">
+          <h1 className="copy-header" >Lizards</h1>
+          Esse nisi proident aute anim incididunt eu quis non. Laboris ut dolore tempor incididunt officia proident.
         </section>
       </article>
     </main>
