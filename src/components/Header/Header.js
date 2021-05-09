46d46e68f649f094b5b603988b40d3d43631bf00
mattdean <matthewallandean@gmail.com>
2021-05-09 11:26:44 -0600

    Add carousel and start animations

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import './Header.css';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+
 
 export default function Header() {
-  return (
-    <header>
-      <h1>
-        Molly Newton Interiors? Designs? 
-      </h1>
-      <section className="taglines">
-        <p className="button">Nice Rooms</p>
-      </section>
-    </header>
-  )
+  return (  <header>
+            <section className="floater">
+              <h1 className="animate__animated animate__bounceInUp">
+                Molly Newton Interiors? Designs?
+              </h1>
+            </section>
+            <Carousel
+              showThumbs={false}
+              autoPlay={true}
+              showStatus={false}
+            >
+                <div>
+                    <img src="https://duetdesigngroup.com/wp-content/uploads/2020/05/Duet-Design-Residence_Kitchen-V2_compressed-e1591207800427.jpg" />
+                    <p className="legend">Home Life</p>
+                </div>
+                <div>
+                    <img src="https://www.studioilse.com/wp-content/uploads/2020/01/07.jpg" />
+                    <p className="legend">Work is Cool</p>
+                </div>
+                <div>
+                    <img src="https://www.studioilse.com/wp-content/uploads/2020/01/07.jpg" />
+                    <p className="legend">Nap Time</p>
+                </div>
+            </Carousel>
+            </header>
+        );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Main.css';
+import 'animate.css/animate.min.css'
 
 export default function Main() {
   return (
