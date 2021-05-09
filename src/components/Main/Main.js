2dea403aa8d5127e88573fd7b1a8a81602d63795
mattdean <matthewallandean@gmail.com>
2021-05-09 10:18:26 -0600

    Set up skeleton and add placeholders

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React from 'react';
+import './Main.css';
 
 export default function Main() {
   return (
     <main>
-      <section>
-        Esse nisi proident aute anim incididunt eu quis non. Laboris ut dolore tempor incididunt officia proident. Ad consectetur ad laboris labore id. Consectetur aliqua nulla velit sit deserunt et. Occaecat sunt velit Lorem consequat consectetur consectetur non sunt sunt. Voluptate amet incididunt excepteur consectetur dolore.
-      </section>
-      <section>
-        Esse nisi proident aute anim incididunt eu quis non. Laboris ut dolore tempor incididunt officia proident. Ad consectetur ad laboris labore id. Consectetur aliqua nulla velit sit deserunt et. Occaecat sunt velit Lorem consequat consectetur consectetur non sunt sunt. Voluptate amet incididunt excepteur consectetur dolore.
-      </section>
+      <article>
+        <section className="copy">
+          Esse nisi proident aute anim incididunt eu quis non. Laboris ut dolore tempor incididunt officia proident. Ad consectetur ad laboris labore id. Consectetur aliqua nulla velit sit deserunt et. Occaecat sunt velit Lorem consequat consectetur consectetur non sunt sunt. Voluptate amet incididunt excepteur consectetur dolore.
+        </section>
+        <section className="project-picture">
+        Learn More
+        </section>
+      </article>
     </main>
   )
 }
